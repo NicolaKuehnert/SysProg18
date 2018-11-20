586652ca35cc905a4fe478216fdd709f0c217a20
Nikita Lötkemann <nloetkemann@fh-bielefeld.de>
2018-11-20 22:04:56 +0100

    cm_memcpy sollte funktionieren

--- include/speicherverwaltung/speicherverwaltung.h
+++ include/speicherverwaltung/speicherverwaltung.h
@@ -5,7 +5,9 @@
 
 #define MEM_POOL_SIZE 4096
 #define MAGIC_INT 0xacdcacdc
-
+#define toMemblockPtr(x) ((memblock *)x)
+#define inRange(x) (toMemblockPtr(x) >= toMemblockPtr(mempool) && toMemblockPtr(x) <= toMemblockPtr(mempool + MEM_POOL_SIZE))
+ 
 typedef struct memblock{
 	size_t size;
 	struct memblock *next;
--- src/speicherverwaltung/main.c
+++ src/speicherverwaltung/main.c
@@ -22,16 +22,21 @@ int main(){
 	printf("%p\n\n",ptr2);
 
 	printf("Call malloc 3\n");
-	ptr=(memblock *)cm_malloc(500);
+	ptr=(memblock *)cm_malloc(30);
 	printf("%p\n\n",ptr);
 
 	printf("Call malloc 4\n");
 	ptr2=(memblock *)cm_malloc(20);
 	printf("%p\n\n",ptr2);
 
+	printf("Call memcpy\n");
+	cm_memcpy(ptr, ptr2, 20);
+	printf("%p\n\n",ptr2);
+	
 	printf("Call free\n");
 	cm_free(ptr);
 	cm_free(ptr2);
+	
 	
 	return 0;
 }
