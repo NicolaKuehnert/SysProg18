bb712b33ee0c15b3f32f0c0ebdd530d18366af19
NicolaKuehnert <nicola.kuehnert@outlook.de>
2018-11-24 03:03:23 -0800

    Read und Count
    Kompiliert

--- include/ringbuffer/ringbuffer.h
+++ include/ringbuffer/ringbuffer.h
@@ -3,5 +3,16 @@
 
 #include <stdlib.h>
 
+typedef struct ring_buffer {
+size_t size, count, head;
+void **elems;
+void (*free_callback)(void *p);
+}ring_buffer;
 
-#endif /* SPEICHERVERWALTUNG_H */
+ring_buffer *init_buffer(const size_t n, void (*f)(void *p));
+void *read_buffer(ring_buffer *cb);
+void write_buffer(ring_buffer *cb, void *data);
+int free_buffer(ring_buffer *cb);
+int count_elements(const ring_buffer *cb);
+
+#endif 
--- src/ringbuffer/ringbuffer.c
+++ src/ringbuffer/ringbuffer.c
@@ -1,12 +1,16 @@
 #include "ringbuffer/ringbuffer.h"
+#include <stdio.h>
 
 ring_buffer *buffer = NULL; 
+
+
 ring_buffer *init_buffer(const size_t n, void (*f)(void *p)) {
 	if(n > 0){
 		buffer = (ring_buffer* )malloc(sizeof(ring_buffer));
 		buffer->elems = (void*)malloc(n* sizeof(void*)); // unsicher ob das der richtige datentyp dafür ist
 		buffer->free_callback = f;
-		buffer->head ,buffer->count = 0;
+		buffer->head = 0;
+		buffer->count = 0;
 		buffer->size = n;
 	}
 	return buffer;
@@ -25,5 +29,33 @@ void write_buffer(ring_buffer *cb, void *data) {
 	printf("Test %p\n", (char*) cb->elems[cb->head]);
 	if(cb->count < cb->size){
 		cb->count++;
+	}
+}
+
+void *read_buffer(ring_buffer *cb){
+	if(cb->count == 0){
+		return NULL;
+	}
+	else{
+		printf("First Element: %p\n", (char*)cb->elems[cb->head]);
+
+		void *ret = cb->elems[cb->head];				//Erstes Element, das noch nicht gelesen wurde
+		cb->head++;							//Setze Head auf das nächste Element
+		cb->count--;							//Verkleinere count, damit der Buffer weiß, wie viele Elemente noch da sind
+		return ret;
+	}
+
+}
+
+int free_buffer(ring_buffer *cb){
+	return 0;
+}
+
+int count_elements(const ring_buffer *cb){
+	if(cb->count == 0){
+		return -1;
+	}
+	else {
+		return cb->count;
 	}
 }
