a655a528c22f9fa45ba3ba91ea76b5a736f17208
NicolaKuehnert <nicola.kuehnert@outlook.de>
2018-12-12 13:12:33 -0800

    SQLite sollte temp speichern

--- src/temperatur/demo.cpp
+++ src/temperatur/demo.cpp
@@ -20,20 +20,20 @@ using namespace std;
 
 int main(){
 	//ring_buffer* buffer = init_buffer(10, free);
-/*
+
 	I_SQLite db = I_SQLite("temperatur.db");
 
 	if(db.exec("CREATE TABLE temperatures(time text DEFAULT CURRENT_TIMESTAMP, temperature text);") == 0);
-*/
+
 	TempSensor sensor = TempSensor(calculateTemperature);
 	int runs = 0;
 	TM1637_setup();
 
-/*
+
 	//SQL Strings zum Speichern der Temperatur
-	std::string sql = "INSERT INTO temperatures(time, Itemperature) VALUES(";
+	std::string sql = "INSERT INTO temperatures(Itemperature) VALUES(";
 	std::string sql2 = ");";
-*/	
+	
 
 	test();
 
@@ -42,6 +42,10 @@ int main(){
 		
 		
 		float temp = sensor.getTemp();
+		sql += temp;
+		sql += sql2;
+		char *ptr = &sql[0u];
+		db.exec(ptr);
 		cout << std::to_string(temp) + "\n";
 		//write_buffer(buffer, &temp);
 		//TM1637_display_number(percent(buffer));
